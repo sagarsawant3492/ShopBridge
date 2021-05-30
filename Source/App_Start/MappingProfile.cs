4821f7afde2e163463049b830496aad506c09d73
sagarsawant3492 <85054144+sagarsawant3492@users.noreply.github.com>
2021-05-30 13:33:43 +0530

    Add files via upload

---
+++ Source/App_Start/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ShopBridge.Dtos;
+using ShopBridge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopBridge.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Product, ProductDto>();
+            Mapper.CreateMap<ProductDto, Product>();
+
+        }
+    }
+}
