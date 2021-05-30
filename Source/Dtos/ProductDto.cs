8c677be35fd821407c44555e8c175143cd9e2101
sagarsawant3492 <85054144+sagarsawant3492@users.noreply.github.com>
2021-05-30 14:07:55 +0530

    Update ProductDto.cs

--- Source/Dtos/ProductDto.cs
+++ Source/Dtos/ProductDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,27 +9,27 @@ namespace ShopBridge.Dtos
     public class ProductDto
     {
 
-        //[Required]
+        [Required]
         public string Name { get; set; }
 
-        //[Required]
+        [Required]
         public string Category { get; set; }
 
-        //[Required]
+        [Required]
         public string ProductCode { get; set; }
         public string Description { get; set; }
 
-        //[Required]
-        //[Range(0, double.MaxValue, ErrorMessage = "Price should not be less than 0")]
+        [Required]
+        [Range(0, double.MaxValue, ErrorMessage = "Price should not be less than 0")]
         public decimal Price { get; set; }
 
-        //[Required]
-        //[Range(0, int.MaxValue, ErrorMessage = "Quantity should not be less than 0")]
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "Quantity should not be less than 0")]
         public int Quantity { get; set; }
         public string ManufacturerCode { get; set; }
         public string ImageContent { get; set; }
 
-        //[Range(0, double.MaxValue, ErrorMessage = "Quantity should not be less than 0")]
+        [Range(0, double.MaxValue, ErrorMessage = "Quantity should not be less than 0")]
         public decimal Weight { get; set; }
         public string WeightUnit { get; set; }
         public DateTime ManufactureDate { get; set; }
